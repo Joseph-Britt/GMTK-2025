972574005eb4373dddc13d8865fb57a239525365
Joseph-Britt <48034902+Joseph-Britt@users.noreply.github.com>
2025-07-31 22:53:56 -0500

    Guard Sprite Sheet

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Player : MonoBehaviour {
 
     [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private Grid wallGrid;
+    [SerializeField] private Tilemap wallTilemap;
     private Rigidbody2D rb;
 
     private void Awake() {
@@ -11,7 +14,13 @@ public class Player : MonoBehaviour {
 
     private void Update() {
         Vector2 moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
-        transform.position += moveSpeed * Time.deltaTime * (Vector3) moveDirection;
+        transform.position += moveSpeed * Time.deltaTime * (Vector3)moveDirection;
+        if (Input.GetMouseButtonDown(0)) {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2Int position = new Vector2Int((int) Mathf.Floor(mousePosition.x), (int) Mathf.Floor(mousePosition.y));
+            Debug.Log(position);
+            Debug.Log(wallTilemap.HasTile((Vector3Int) position));
+        }
     }
 
 }
