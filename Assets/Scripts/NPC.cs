0578d7318c98a4347893aa5ecfb53c320fa9d609
Isaac-Porter <116916200+Isaac-Porter@users.noreply.github.com>
2025-08-01 21:41:13 -0500

    More animation

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private Animator animator;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    void north(int n)
+    {
+        //animator.SetInt();
+    }
+}
